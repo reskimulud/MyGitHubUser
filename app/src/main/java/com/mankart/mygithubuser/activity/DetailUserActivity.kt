53775e26af73cb016953d99ba8b6919e263ab5ce
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-03-03 20:02:45 +0700

    creat tablayout to detail profile

--- app/src/main/java/com/mankart/mygithubuser/activity/DetailUserActivity.kt
+++ app/src/main/java/com/mankart/mygithubuser/activity/DetailUserActivity.kt
@@ -2,9 +2,14 @@ package com.mankart.mygithubuser.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mankart.mygithubuser.R
+import com.mankart.mygithubuser.adapter.FollowTabPagerAdapter
 import com.mankart.mygithubuser.databinding.ActivityDetailUserBinding
 import com.mankart.mygithubuser.model.UsersModel
 import java.text.DecimalFormat
@@ -15,6 +20,12 @@ class DetailUserActivity : AppCompatActivity() {
 
     companion object {
         const val PUT_EXTRA = "put_extra"
+
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.content_tab_follower,
+            R.string.content_tab_following
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +48,14 @@ class DetailUserActivity : AppCompatActivity() {
         binding.tvFollowing.text = getDecimal(data.following)
         binding.tvCompany.text = data.company
         binding.tvLocation.text = data.location
+
+        val followTabPagerAdapter = FollowTabPagerAdapter(this)
+        val viewPager: ViewPager2 = binding.viewPager
+        viewPager.adapter = followTabPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
 
         supportActionBar?.hide()
     }
---
+++ app/src/main/java/com/mankart/mygithubuser/adapter/FollowTabPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.mankart.mygithubuser.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.mankart.mygithubuser.fragment.FollowerFragment
+import com.mankart.mygithubuser.fragment.FollowingFragment
+
+class FollowTabPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position) {
+            0 -> fragment = FollowerFragment()
+            1 -> fragment = FollowingFragment()
+        }
+        return fragment as Fragment
+    }
+
+}
